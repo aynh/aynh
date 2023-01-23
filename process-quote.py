b9410199d8eae6665baaf98ec94e210dbb56ac91
Aynh <99479536+Aynh@users.noreply.github.com>
2023-01-23 17:49:51 +0800

    Update `Update Quote`

--- process-quote.py
+++ process-quote.py
@@ -13,16 +13,12 @@ print("> {}".format(quoted(quote)), end="\n\n")
 
 print("&mdash;", end=" ")
 
-character_query = " AND ".join(character.split(" "))
-character_url = (
-    "https://anidb.net/search/fulltext/?adb.search={}&do.search=1&entity.chartb=1".format(
-        urllib.parse.quote(character_query.encode("utf-8"))
-    )
+character_url = "https://myanimelist.net/character.php?q={}&cat=character".format(
+    urllib.parse.quote(character.encode("utf-8"))
 )
 print("[{}]({})".format(bold(character), character_url), end=", ")
 
-show_query = " AND ".join(show.split(" "))
-show_url = "https://anidb.net/search/fulltext/?adb.search={}&do.search=1&entity.animetb=1".format(
-    urllib.parse.quote(show_query.encode("utf-8"))
+show_url = "https://myanimelist.net/search/all?q={}&cat=all".format(
+    urllib.parse.quote(show.encode("utf-8"))
 )
 print("[{}]({})".format(bold(show), show_url))
