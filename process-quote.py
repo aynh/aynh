93d5d4f043feaa4cc0650d2ab5456fe280796227
Aynh <99479536+Aynh@users.noreply.github.com>
2023-01-23 16:46:56 +0800

    Update

--- process-quote.py
+++ process-quote.py
@@ -5,6 +5,7 @@ import urllib.parse
 
 character, show, quote = sys.argv[1:]
 
+bold = lambda s: f"**{s}**"
 quoted = lambda s: f'"{s}"'
 
 print()
@@ -12,10 +13,16 @@ print(f"> {quoted(quote)}", end="\n\n")
 
 print("&mdash;", end=" ")
 
-urlencoded_character = urllib.parse.quote(quoted(character).encode("utf-8"))
-character_url = f"https://anidb.net/search/fulltext/?adb.search={urlencoded_character}&do.search=1&entity.chartb=1"
-print(f"[**{character}**]({character_url})", end=", ")
+character_query = " AND ".join(character.split(" "))
+character_url = (
+    "https://anidb.net/search/fulltext/?adb.search={}&do.search=1&entity.chartb=1".format(
+        urllib.parse.quote(character_query.encode("utf-8"))
+    )
+)
+print(f"[{bold(character)}]({character_url})", end=", ")
 
-urlencoded_show = urllib.parse.quote(quoted(show).encode("utf-8"))
-show_url = f"https://anidb.net/search/fulltext/?adb.search={urlencoded_show}&do.search=1&entity.animetb=1&"
-print(f"[**{show}**]({show_url})")
+show_query = " AND ".join(show.split(" "))
+show_url = "https://anidb.net/search/fulltext/?adb.search={}&do.search=1&entity.animetb=1&".format(
+    urllib.parse.quote(show_query.encode("utf-8"))
+)
+print(f"[{bold(show)}]({show_url})")
